19312af0fcedc0865ddaa23a3f0780eeea19f237
JulieC35 <chapdelainejulie0@gmail.com>
2020-02-21 17:56:10 +0100

    Ajout waitgame et init routes

---
+++ src/app/wait-game/wait-game.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wait-game',
+  templateUrl: './wait-game.component.html',
+  styleUrls: ['./wait-game.component.css']
+})
+export class WaitGameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
