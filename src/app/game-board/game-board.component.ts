0a137893e2e2f52f3e4889f6035cec0c1ee36060
JulieC35 <chapdelainejulie0@gmail.com>
2020-03-25 16:04:46 +0100

    Correction + ajout fin de partie

--- src/app/game-board/game-board.component.ts
+++ src/app/game-board/game-board.component.ts
@@ -17,6 +17,7 @@ export class GameBoardComponent implements OnInit {
 
   @Input() boardGame;
   @Input() boardContainer;
+  @Input() message: string;
   board;
 
   yourTurn: boolean;
@@ -47,13 +48,16 @@ export class GameBoardComponent implements OnInit {
             const message = datas.slice(1);
             switch (message) {
               case 'AWAITING':
+                document.getElementById('message').innerText = '';
                 this.yourTurn = false;
                 break;
               case 'START':
                 console.log('Début de la partie !');
+                document.getElementById('message').innerText = 'Début de la partie !';
                 break;
               case 'READY':
                 this.yourTurn = true;
+                document.getElementById('message').innerText = '';
                 break;
               case 'WIN':
                 console.log('Vous avez gagné !');
@@ -101,7 +105,19 @@ export class GameBoardComponent implements OnInit {
             console.log('capturés: ' + captures);
             break;
           case '!':
+            if (datas.slice(1).indexOf('Position not allowed') != -1) {
+              const coord = datas.slice(1).split(':')[1];
+              console.log(datas.slice(1));
+              document.getElementById(coord).classList.remove('active');
+              if (joueur == 'J1') {
+                document.getElementById(coord).classList.remove('black');
+              } else {
+                document.getElementById(coord).classList.remove('white');
+              }
+              this.yourTurn = true;
+            }
             console.log(datas.slice(1));
+            document.getElementById('message').innerText = datas.slice(1);
             // document.getElementById(coord).classList.remove('active');
             break;
           default:
@@ -117,6 +133,7 @@ export class GameBoardComponent implements OnInit {
     for (let i = 0; i < stones.length; i++) {
       this.getStoneClickPromise(stones[i]).then(
         data => {
+          console.log('passage: ' + data);
           if (this.yourTurn) {
             const classname = data[0];
             const target = data[1];
@@ -136,6 +153,7 @@ export class GameBoardComponent implements OnInit {
     return new Promise((resolve: PromiseResolve<any>, reject: PromiseReject): void => {
       // tslint:disable-next-line:only-arrow-functions
       stone.onclick = (event) => {
+        console.log(event);
         if (!event.target.getAttribute('class').includes('active')) {
           let classname;
           // tslint:disable-next-line:triple-equals
@@ -145,6 +163,7 @@ export class GameBoardComponent implements OnInit {
             classname = 'active white';
           }
           const data = [classname, event.target, event.target.id];
+          console.log('Passage au bon endroit');
           resolve(data);
         } else {
           reject('Pierre déjà placée !');
@@ -152,211 +171,6 @@ export class GameBoardComponent implements OnInit {
       };
     });
   }
-
-  /*createColums() {
-    let htmlBoardCoordH = '<div class="board-coord-bar horizontal">';
-
-    for (let i = 0; i < 12; i++) {
-      htmlBoardCoordH += '<div class="board-coord">' + this.columns[i] + '</div>';
-    }
-    htmlBoardCoordH += '</div>';
-    document.getElementById('boardGame').innerHTML += htmlBoardCoordH;
-    let htmlColumnLayer = '<div class="board-layer">';
-    for (let i = 0; i < 12; i++) {
-      htmlColumnLayer += '<div class="board-column">';
-      for (let j = 0; j < 11; j++) {
-        htmlColumnLayer += '<div class="board-verticalLine"></div>';
-      }
-      htmlColumnLayer += '</div>';
-    }
-    htmlColumnLayer += '</div>';
-
-    // Ajout du code à l'intérieur de la balise boardGame
-    document.getElementById('boardGame').innerHTML += (htmlColumnLayer);
-  }
-
-  createLines() {
-    let htmlRowLayer = '<div class="board-layer fldc">';
-    for (let i = 0; i < 12; i++) {
-      htmlRowLayer += '<div class="board-row">';
-      for (let j = 0; j < 11; j++) {
-        htmlRowLayer += '<div class="board-horizonalLine"></div>';
-      }
-      htmlRowLayer += '</div>';
-    }
-    htmlRowLayer += '</div>';
-    // Ajout du code à l'intérieur de la balise boardGame
-    document.getElementById('boardGame').innerHTML += htmlRowLayer;
-  }
-
-  createDiamondCenter() {
-    let htmlDiamondCenterLayer = '<div class="board-layer fldc">';
-    for (let i = 0; i < 12; i++) {
-      if (i === 0) {
-        htmlDiamondCenterLayer += '<div class="board-diamondCenter-row' + (i === 0 ? ' first' : '') + '"></div>';
-      } else {
-        htmlDiamondCenterLayer += '<div class="board-diamondCenter-row">';
-
-        let nbLosange = 6;
-        if (i % 2 === 0) {
-          nbLosange = 5;
-        }
-
-        for (let j = 0; j < nbLosange; j++) {
-          htmlDiamondCenterLayer += '<div class="board-diamondCenter-line"></div>';
-        }
-        htmlDiamondCenterLayer += '</div>';
-      }
-    }
-    htmlDiamondCenterLayer += '</div>';
-
-    // Ajout du code à l'intérieur de la balise boardGame
-    document.getElementById('boardGame').innerHTML += htmlDiamondCenterLayer;
-  }
-
-  createSquareCenter() {
-    // Création des diagonales des carrés
-    let htmlSquareCenterLayer = '<div class="board-layer fldc">';
-    for (let i = 0; i < 11; i++) {
-      htmlSquareCenterLayer += '<div class="board-squareCenter-row">';
-
-      let nbCarres = 6;
-      if (i % 2 === 0) {
-        nbCarres = 5;
-      }
-
-      for (let j = 0; j < nbCarres; j++) {
-        htmlSquareCenterLayer += '<div class="board-squareCenter-line"></div>';
-        htmlSquareCenterLayer += '<div class="board-squareCenter-line"></div>';
-      }
-      htmlSquareCenterLayer += '</div>';
-    }
-
-    htmlSquareCenterLayer += '</div>';
-
-    // Ajout du code à l'intérieur de la balise boardGame
-    document.getElementById('boardGame').innerHTML += htmlSquareCenterLayer;
-  }
-
-  addColor() {
-    // Ajout des carrés de couleur
-    let htmlSquareColorLayer = '<div class="board-layer fldc color">';
-    for (let i = 0; i < 11; i++) {
-      htmlSquareColorLayer += '<div class="board-squareColor-row">';
-
-      let nbCarres = 6;
-      if (i % 2 === 0) {
-        nbCarres = 5;
-      }
-
-      for (let j = 0; j < nbCarres; j++) {
-        htmlSquareColorLayer += '<div class="board-squareColor-square"></div>';
-      }
-      htmlSquareColorLayer += '</div>';
-    }
-
-    htmlSquareColorLayer += '</div>';
-
-    // Ajout du code à l'intérieur de la balise boardGame
-    document.getElementById('boardGame').innerHTML += htmlSquareColorLayer;
-  }
-
-  addStones() {
-    // Ajout des pierres
-    let htmlStoneLayer = '<div class="board-layer fldc stone">';
-
-    let indexRows = 0;
-    let indexColumns = 0;
-    for (let i = 0; i < 6; i++) {
-      // Ligne sans offset
-      htmlStoneLayer += '<div class="boardGame-stones-row">';
-      for (let j = 0; j < 12; j++) {
-        const id = this.columns[j] + ',' + this.rows[indexRows];
-        htmlStoneLayer += '<div class="boardGame-stone" id="' + id + '"></div>';
-      }
-      htmlStoneLayer += '</div>';
-
-      // Ligne square
-      htmlStoneLayer += '<div class="boardGame-stones-row squareCenter">';
-      indexColumns = 1;
-      for (let j = 0; j < 5; j++) {
-        const id = this.columns[indexColumns] + '-' + this.columns[indexColumns + 1] + ','
-          + this.rows[indexRows + 1] + '-' + this.rows[indexRows];
-        htmlStoneLayer += '<div class="boardGame-stone" id="' + id + '"></div>';
-        indexColumns += 2;
-      }
-      htmlStoneLayer += '</div>';
-
-      indexRows++;
-
-      // Ligne sans offset
-      htmlStoneLayer += '<div class="boardGame-stones-row offset">';
-      for (let j = 0; j < 12; j++) {
-        const id = this.columns[j] + ',' + this.rows[indexRows];
-        htmlStoneLayer += '<div class="boardGame-stone " id="' + id + '"></div>';
-      }
-      htmlStoneLayer += '</div>';
-
-      if (i < 5) {
-        // Ligne square
-        htmlStoneLayer += '<div class="boardGame-stones-row squareCenter no-offset">';
-        indexColumns = 0;
-        for (let j = 0; j < 6; j++) {
-          const id = this.columns[indexColumns] + '-' + this.columns[indexColumns + 1] + ','
-            + this.rows[indexRows] + '-' + this.rows[indexRows + 1];
-          htmlStoneLayer += '<div class="boardGame-stone" id="' + id + '"></div>';
-          indexColumns += 2;
-        }
-        htmlStoneLayer += '</div>';
-      }
-
-      indexRows++;
-
-    }
-
-    htmlStoneLayer += '</div>';
-
-    // Ajout du code à l'intérieur de la balise boardGame
-    document.getElementById('boardGame').innerHTML += htmlStoneLayer;
-  }
-
-  addCoordBeforeBoard() {
-    let htmlBoardCoord = '<div class="board-coord-bar">';
-    for (let i = 0; i < 12; i++) {
-      htmlBoardCoord += '<div class="board-coord">' + this.rows[i] + '</div>';
-    }
-    htmlBoardCoord += '</div>';
-    document.getElementById('boardContainer').innerHTML += htmlBoardCoord;
-    const board = '<div class="board" id="boardGame" >\n' +
-      '  </div>';
-    document.getElementById('boardContainer').innerHTML += board;
-  }
-
-  addCoord() {
-    // Ajout des coordonnées sur le côté du plateau
-    let htmlBoardCoord = '<div class="board-coord-bar">';
-    for (let i = 0; i < 12; i++) {
-      htmlBoardCoord += '<div class="board-coord">' + this.rows[i] + '</div>';
-    }
-    htmlBoardCoord += '</div>';
-
-    // Ajout du code à l'intérieur de la balise boardContainer
-    // this.boardContainer.prepend(htmlBoardCoord);
-    document.getElementById('boardContainer').innerHTML += htmlBoardCoord;
-
-
-    // Ajout des coordonnées sur le bas et haut du plateau
-    let htmlBoardCoordH = '<div class="board-coord-bar horizontal">';
-
-    for (let i = 0; i < 12; i++) {
-      htmlBoardCoordH += '<div class="board-coord">' + this.columns[i] + '</div>';
-    }
-    htmlBoardCoordH += '</div>';
-
-    // Ajout du code à l'intérieur de la balise boardGame
-    // this.boardGame.prepend(htmlBoardCoordH);
-    document.getElementById('boardGame').innerHTML += htmlBoardCoordH;
-  } */
 
   stoneClick(event: any) {
     console.log('stone click');
--- src/app/party-win/party-win.component.ts
+++ src/app/party-win/party-win.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-party-win',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PartyWinComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  accueil() {
+    this.router.navigate(['']);
   }
 
 }
